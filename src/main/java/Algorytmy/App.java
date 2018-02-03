5323fdfc611a6833580b9a4640b6eeccf3c37843
Astropl <30636834+Astropl@users.noreply.github.com>
2018-02-03 10:07:56 +0100

    pierwszy

---
+++ src/main/java/Algorytmy/App.java
@@ -0,0 +1,13 @@
+package Algorytmy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
